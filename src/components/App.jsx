953881b7983b42ac3bc7daf52d0257addc668f0c
Olha-PSM <Olha84pismennaa@gmail.com>
2023-12-19 16:19:22 +0200

    update task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,8 +4,8 @@ import { ImageGallery } from './ImageGallery/ImageGallery';
 import { Button } from './Button/Button';
 import { Searchbar } from './Searchbar/Searchbar';
 import { Modal } from './Modal/Modal';
-import { toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+// import { toast } from 'react-toastify';
+// import 'react-toastify/dist/ReactToastify.css';
 import { Section } from './Appi.styled';
 
 import { Loader } from './Loader/Loader';
